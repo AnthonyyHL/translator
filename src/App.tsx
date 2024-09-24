dc049b675927dacafc74100a110ee780d34d5d3a
AnthonyyHL <anthleon@espol.edu.ec>
2024-09-24 17:32:03 -0500

    feat: Add language switch functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useReducer } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './App.css'
 
+const intialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  translatedText: '',
+  loading: false,
+}
+
+function reducer(state, action) {
+  const { type } = action
+
+  switch (type) {
+    case 'SWITCH_LANGUAGES':
+      return {
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage,
+      }
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, intialState)
+
+  // proof
+  const switchLanguages = () => {
+    dispatch({ type: 'SWITCH_LANGUAGES' })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Translator clone</h1>
+      <span>fromLanguage {state.fromLanguage}</span>
+      <span>toLanguage {state.toLanguage}</span>
+      <button onClick={switchLanguages}>Switch</button>
     </>
   )
 }
